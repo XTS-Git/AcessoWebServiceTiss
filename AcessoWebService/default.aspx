be08d50735c10c05a5805e6d34f061e9964ded7a
Ricardo Costa <exatos.lee@outlook.com>
2020-05-18 14:53:51 -0300

    adicionando bootstrap/jquery

--- AcessoWebService/default.aspx
+++ AcessoWebService/default.aspx
@@ -5,40 +5,125 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title>Exemplo de acesso webservice TISS ANS</title>
+    <title>Acesso webservice TISS/ANS</title>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <link rel="stylesheet" href="./css/bootstrap.css"  />
+
+
+    <script src="./js/jquery-3.4.1.js"></script>
+    <script src="./js/bootstrap.js"></script>
 
 </head>
 <body>
     <form id="form1" runat="server">
-        <div>
-            <button id="btnEnvia">
-                envia
-            </button>
+        <div class="container">
+                <h2 class="card-title">TISS/ANS</h2>
+                    <label for="numeroCarteira">Número Carteira</label>
+                    <input type="text" class="form-control" id="numeroCarteira" /> 
+
+                    <label for="sequencial">Sequencial ( gerado automaticamente )</label>
+                    <input type="text" class="form-control" id="sequencial" /> 
+
+                    <label for="codigoProcedimento">Código Procedimento</label>
+                    <input type="text" class="form-control" id="codigoProcedimento" /> 
+
+                    <label for="descricaoProcedimento">descrição procedimento</label>
+                    <input type="text" class="form-control" id="descricaoProcedimento" /> 
+
+                    <label for="codigoPrestadorNaOperadora">Código Prestador</label>
+                    <input type="text" class="form-control" id="codigoPrestadorNaOperadora" /> 
+
+                    <label for="registroANS">Registro ANS</label>
+                    <input type="text" class="form-control" id="registroANS" /> 
+
+
+                <br />
+                <input type="button" id="btnEnvia" class="btn"  value="envia solicitação" />
+                <input type="button" id="btnRecebeXML" class="btn" value="Gera XML de envio" />
+             </div>
+        <br />
+        <div class="container" style="border:solid 1px #000;">
+            <table class="table">
+             <thead>
+                <tr>
+                  <th scope="col">XML Retorno</th>
+                  <th scope="col">XML Gerado para envio ao webService</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                    <td style="width:50%;">
+                        <span id="retornoAutorizacaoXML" ></span>
+                    </td>
+                    <td >
+                        <span id="retornaXMLExemplo"></span>
+                    </td>
+                </tr>
+              </tbody>
+            </table>
         </div>
     </form>
 
     <script>
         $(function() {
-            $('#btnEnvia').click(function () { 
-                $.ajax({
-                    url: 'default.aspx/enviaSolicitacao',
-                    data: "{numerocarteira:'12345', sequencial:'565656', codigoProcedimento:'101010', descricaoProcedimento:'Teste descrição',  codigoPrestadorNaOperadora:'909090', registroANS:'00000'}",
-                    type: "POST",
-                    contentType: "application/json; charset=utf-8",
-                    dataType: "json",
-                    async: true,
-                    global: false,
-                    success: function (response) {
 
-                    },
-                    error: (function Error(request, settings, error) {
-                        alert('Erro ajaxClass: ' + settings.url + ' ' + ' Erro: ' + error + ' text: ' + request.responseText);
+            $('#sequencial').val(getRandom());
+
+            $('#btnRecebeXML').click(function () {
+                try {
+                    $.ajax({
+                        url: 'default.aspx/geraXML',
+                        data: "{numerocarteira:'" + $('#numeroCarteira').val() + "', sequencial:'" + $('#sequencial').val() + "', codigoProcedimento:'" + $('#codigoProcedimento').val() + "', descricaoProcedimento:'" + $('#descricaoProcedimento').val() + "',  codigoPrestadorNaOperadora:'" + $('#codigoPrestadorNaOperadora').val() + "', registroANS:'" + $('#registroANS').val() + "'}",
+                        type: "POST",
+                        contentType: "application/json; charset=utf-8",
+                        dataType: "json",
+                        async: true,
+                        success: function (response) {
+                            if (response) {
+                                $('#retornaXMLExemplo').text(response.d);
+                            } else {
+                                $('#retornaXMLExemplo').html('Não retornou nada');
+                            }
+                        },
+                        error: (function Error(request, settings, error) {
+                            $('#retornaXMLExemplo').html('Erro ajaxClass: ' + settings.url + ' ' + ' Erro: ' + error + ' text: ' + request.responseText);
+                        })
                     })
-                })
+                } catch (e) {
+                    $('#retornaXMLExemplo').html(e);
+                }
+            });
+
+            $('#btnEnvia').click(function () { 
+                try {
+                    $.ajax({
+                        url: 'default.aspx/enviaSolicitacao',
+                        data: "{numerocarteira:'" + numeroCarteira + "', sequencial:'" + sequencial + "', codigoProcedimento:'" + codigoProcedimento + "', descricaoProcedimento:'" + descricaoProcedimento + "',  codigoPrestadorNaOperadora:'" + codigoPrestadorNaOperadora + "', registroANS:'" + registroANS + "'}",
+                        type: "POST",
+                        contentType: "application/json; charset=utf-8",
+                        dataType: "json",
+                        async: true,
+                        success: function (response) {
+                            if (response) {
+                                $('#retornoAutorizacaoXML').html(response.d);
+                            } else {
+                                $('#retornoAutorizacaoXML').html('Não retornou nada');
+                            }
+                        },
+                        error: (function Error(request, settings, error) {
+                            $('#retornoAutorizacaoXML').html('Erro ajaxClass: ' + settings.url + ' ' + ' Erro: ' + error + ' text: ' + request.responseText);
+                        })
+                    })
+                } catch (e) {
+                    $('#retornoAutorizacaoXML').html(e);
+                }
             })
         });
+
+        function getRandom() {
+            return Math.floor(Math.random() * 10000);
+        }
+
     </script>
 </body>
 </html>
--- AcessoWebService/default.aspx.cs
+++ AcessoWebService/default.aspx.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Services;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml.Serialization;
 using AcessoWebService.Localhost5000TissSolicitacaoProcedimento;
 
 namespace AcessoWebService
@@ -20,84 +23,163 @@ namespace AcessoWebService
         public static string enviaSolicitacao(string numerocarteira, string sequencial, string codigoProcedimento, string descricaoProcedimento,
                                               string codigoPrestadorNaOperadora, string registroANS)
         {
-            solicitacaoProcedimentoWS solicitacao = new solicitacaoProcedimentoWS();
-            solicitacao.cabecalho = new cabecalhoTransacao();
-            solicitacao.cabecalho.identificacaoTransacao = new cabecalhoTransacaoIdentificacaoTransacao();
-            solicitacao.cabecalho.identificacaoTransacao.tipoTransacao = dm_tipoTransacao.SOLICITACAO_PROCEDIMENTOS;
-            solicitacao.cabecalho.identificacaoTransacao.sequencialTransacao = sequencial; // número que identifica a transação
-            solicitacao.cabecalho.identificacaoTransacao.dataRegistroTransacao = DateTime.Now.Date;
-            solicitacao.cabecalho.identificacaoTransacao.horaRegistroTransacao = DateTime.Now;
-            #region ORIGEM
-            cabecalhoTransacaoOrigemIdentificacaoPrestador origem = new cabecalhoTransacaoOrigemIdentificacaoPrestador();
-            origem.ItemElementName = ItemChoiceType.codigoPrestadorNaOperadora;
-            origem.Item = codigoPrestadorNaOperadora;
-            solicitacao.cabecalho.origem = new cabecalhoTransacaoOrigem();
-            solicitacao.cabecalho.origem.Item = origem;
-            #endregion ORIGEM
-
-            #region DESTINO
-            //cabecalhoTransacaoDestino destino = new cabecalhoTransacaoDestino();
-            //destino.Item
-
-            solicitacao.cabecalho.destino = new cabecalhoTransacaoDestino();
-            solicitacao.cabecalho.destino.Item = registroANS;
+            solicitacaoProcedimentoWS solicitacao = empacotaDados(numerocarteira, sequencial, codigoProcedimento, descricaoProcedimento,
+                                                                  codigoPrestadorNaOperadora, registroANS);
 
 
-            #endregion DESTINO
-
-            solicitacao.cabecalho.Padrao = dm_versao.Item30303;
-
-            #region PROCEDIMENTOS
-            List<ctm_spsadtSolicitacaoGuiaProcedimentosSolicitados> listProcedimento = new List<ctm_spsadtSolicitacaoGuiaProcedimentosSolicitados>();
-
-            ctm_spsadtSolicitacaoGuiaProcedimentosSolicitados procedimento = new ctm_spsadtSolicitacaoGuiaProcedimentosSolicitados();
-            procedimento.procedimento = new ct_procedimentoDados();
-            procedimento.procedimento.codigoProcedimento = codigoProcedimento;
-            procedimento.procedimento.descricaoProcedimento = descricaoProcedimento;
-            procedimento.procedimento.codigoTabela = dm_tabela.Item00;
-            listProcedimento.Add(procedimento);
-
-            ctm_spsadtSolicitacaoGuia guiaSADT = new ctm_spsadtSolicitacaoGuia();
-            guiaSADT.procedimentosSolicitados = listProcedimento.ToArray();
-
-
-            ct_solicitacaoProcedimento ctSoliciatacao = new ct_solicitacaoProcedimento();
-            ctm_spsadtSolicitacaoGuia sadtGuia = new ctm_spsadtSolicitacaoGuia();
-
-
-            // sadtGuia.dadosBeneficiario
-            //ctSoliciatacao.Item
-            //ctSoliciatacao.Item.
-
-            // solicitacao.solicitacaoProcedimento = ct_solicitacaoProcedimento();
-
-            #endregion PRTOCEDIMENTOS
 
             try
             {
                 tissSolicitacaoProcedimento_BindingClient envia = new tissSolicitacaoProcedimento_BindingClient();
                 autorizacaoProcedimentoWS retorno = envia.tissSolicitacaoProcedimento_Operation(solicitacao);
-                retorno = retorno;
+                return GetRetornoXmlString(retorno);
             }
             catch (Exception ex)
             {
-                throw;
+                return ex.Message;
+            }
+        }
+
+        [WebMethod]
+        public static string geraXML(string numerocarteira, string sequencial, string codigoProcedimento, string descricaoProcedimento,
+                                              string codigoPrestadorNaOperadora, string registroANS)
+        {
+            try
+            {
+
+                solicitacaoProcedimentoWS solicitacao = empacotaDados(numerocarteira, sequencial, codigoProcedimento, descricaoProcedimento,
+                                                                      codigoPrestadorNaOperadora, registroANS);
+                string resposta = GetEnvioXmlString(solicitacao);;
+                return  resposta;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
             }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-            return null;
         }
+
+
+
+        public static solicitacaoProcedimentoWS empacotaDados(string numerocarteira, string sequencial, string codigoProcedimento, string descricaoProcedimento,
+                                              string codigoPrestadorNaOperadora, string registroANS)
+        {
+            try
+            {
+                solicitacaoProcedimentoWS solicitacao = new solicitacaoProcedimentoWS();
+                solicitacao.cabecalho = new cabecalhoTransacao();
+                solicitacao.cabecalho.identificacaoTransacao = new cabecalhoTransacaoIdentificacaoTransacao();
+                solicitacao.cabecalho.identificacaoTransacao.tipoTransacao = dm_tipoTransacao.SOLICITACAO_PROCEDIMENTOS;
+                solicitacao.cabecalho.identificacaoTransacao.sequencialTransacao = sequencial; // número que identifica a transação
+                solicitacao.cabecalho.identificacaoTransacao.dataRegistroTransacao = DateTime.Now.Date;
+                solicitacao.cabecalho.identificacaoTransacao.horaRegistroTransacao = DateTime.Now;
+                #region ORIGEM
+                cabecalhoTransacaoOrigemIdentificacaoPrestador origem = new cabecalhoTransacaoOrigemIdentificacaoPrestador();
+                origem.ItemElementName = ItemChoiceType.codigoPrestadorNaOperadora;
+                origem.Item = codigoPrestadorNaOperadora;
+                solicitacao.cabecalho.origem = new cabecalhoTransacaoOrigem();
+                solicitacao.cabecalho.origem.Item = origem;
+                #endregion ORIGEM
+
+                #region DESTINO
+                //cabecalhoTransacaoDestino destino = new cabecalhoTransacaoDestino();
+                //destino.Item
+
+                solicitacao.cabecalho.destino = new cabecalhoTransacaoDestino();
+                solicitacao.cabecalho.destino.Item = registroANS;
+
+
+                #endregion DESTINO
+
+                solicitacao.cabecalho.Padrao = dm_versao.Item30303;
+
+                #region PROCEDIMENTOS
+                List<ctm_spsadtSolicitacaoGuiaProcedimentosSolicitados> listProcedimento = new List<ctm_spsadtSolicitacaoGuiaProcedimentosSolicitados>();
+
+                ctm_spsadtSolicitacaoGuiaProcedimentosSolicitados procedimento = new ctm_spsadtSolicitacaoGuiaProcedimentosSolicitados();
+                procedimento.procedimento = new ct_procedimentoDados();
+                procedimento.procedimento.codigoProcedimento = codigoProcedimento;
+                procedimento.procedimento.descricaoProcedimento = descricaoProcedimento;
+                procedimento.procedimento.codigoTabela = dm_tabela.Item00;
+                listProcedimento.Add(procedimento);
+
+                ctm_spsadtSolicitacaoGuia guiaSADT = new ctm_spsadtSolicitacaoGuia();
+                guiaSADT.procedimentosSolicitados = listProcedimento.ToArray();
+
+
+                ct_solicitacaoProcedimento ctSoliciatacao = new ct_solicitacaoProcedimento();
+                ctm_spsadtSolicitacaoGuia sadtGuia = new ctm_spsadtSolicitacaoGuia();
+
+
+                // sadtGuia.dadosBeneficiario
+                //ctSoliciatacao.Item
+                //ctSoliciatacao.Item.
+
+                // solicitacao.solicitacaoProcedimento = ct_solicitacaoProcedimento();
+                #endregion PROCEDIMENTOS
+
+                return solicitacao;
+            }
+            catch (Exception ex)
+            {
+                throw; // para teste
+            }
+        }
+
+
+
+        #region
+        public static string GetRetornoXmlString(autorizacaoProcedimentoWS doc)
+        {
+            var ns = new XmlSerializerNamespaces();
+            ns.Add("ds", "http://www.w3.org/2000/09/xmldsig#");
+            ns.Add("ans", "http://www.ans.gov.br/padroes/tiss/schemas");
+            var serializer = new XmlSerializer(doc.GetType());
+
+            using (var writer = new Util.StringWriterUTF8())
+            {
+                serializer.Serialize(writer, doc, ns);
+
+                return writer.ToString();
+            }
+        }
+
+
+        public static string GetEnvioXmlString(solicitacaoProcedimentoWS doc)
+        {
+            var ns = new XmlSerializerNamespaces();
+            ns.Add("ds", "http://www.w3.org/2000/09/xmldsig#");
+            ns.Add("ans", "http://www.ans.gov.br/padroes/tiss/schemas");
+            var serializer = new XmlSerializer(doc.GetType());
+
+            using (var writer = new Util.StringWriterUTF8())
+            {
+                serializer.Serialize(writer, doc, ns);
+
+                return writer.ToString();
+            }
+        }
+
+        #endregion
+
+
     }
+
+
+    public static class Util
+    {
+        public sealed class StringWriterIso : StringWriter
+        {
+            public override Encoding Encoding { get { return Encoding.GetEncoding("ISO-8859-1"); } }
+        }
+
+        public sealed class StringWriterUTF8 : StringWriter
+        {
+            public override Encoding Encoding { get { return Encoding.GetEncoding("UTF-8"); } }
+        }
+
+
+    }
+
 }
+
